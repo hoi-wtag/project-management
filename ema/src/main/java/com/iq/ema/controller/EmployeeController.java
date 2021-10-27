c8d24124bc0288466d9e6219a2f1a94c81fb5ff0
Iqbal <iqbal.hossain@welldev.io>
2021-10-27 15:45:37 +0600

    introduce service layer

--- ema/src/main/java/com/iq/ema/controller/EmployeeController.java
+++ ema/src/main/java/com/iq/ema/controller/EmployeeController.java
@@ -2,7 +2,7 @@ package com.iq.ema.controller;
 
 import com.iq.ema.exceptions.ResourceNotFoundException;
 import com.iq.ema.model.Employee;
-import com.iq.ema.repository.EmployeeRepository;
+import com.iq.ema.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,7 @@ import java.util.Map;
 public class EmployeeController {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
 //    public EmployeeController(EmployeeRepository employeeRepository) {
 //        this.employeeRepository = employeeRepository;
@@ -27,7 +27,7 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
 
-        return employeeRepository.findAll();
+        return (List<Employee>) employeeService.getAll();
     }
 
     // create employee restapi
@@ -35,14 +35,14 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody  Employee employee){
 
-        return employeeRepository.save(employee);
+        return employeeService.save(employee);
     }
 
     // get employee by id rest api
 
     @GetMapping("/employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
-        Employee employee= employeeRepository.findById(id)
+        Employee employee= employeeService.findByEmployeeId(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"+id));
         return ResponseEntity.ok(employee);
     }
@@ -51,23 +51,23 @@ public class EmployeeController {
 
     @PutMapping("/employees/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable long id,@RequestBody  Employee employeeDetails){
-        Employee employee= employeeRepository.findById(id)
+        Employee employee= employeeService.findByEmployeeId(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"+id));
         employee.setFirstName(employeeDetails.getFirstName());
         employee.setLastName(employeeDetails.getLastName());
         employee.setEmailId(employeeDetails.getEmailId());
 
-        Employee updateEmployee= employeeRepository.save(employee);
+        Employee updateEmployee= employeeService.save(employee);
         return ResponseEntity.ok(updateEmployee);
     }
 
     // delete employee rest api
     @DeleteMapping("/employees/{id}")
     public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
-        Employee employee= employeeRepository.findById(id)
+        Employee employee= employeeService.findByEmployeeId(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"+id));
 
-        employeeRepository.delete(employee);
+        employeeService.delete(employee);
         Map<String,Boolean> response = new HashMap<>();
         response.put("Deleted",Boolean.TRUE);
         return ResponseEntity.ok(response);
---
+++ ema/src/main/java/com/iq/ema/service/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.iq.ema.service;
+
+import com.iq.ema.model.Employee;
+import com.iq.ema.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    EmployeeRepository empRepo;
+
+    public Iterable<Employee> getAll() {
+        return empRepo.findAll();
+    }
+
+    public Employee save(Employee employee) {
+        return empRepo.save(employee);
+    }
+
+    public Optional<Employee> findByEmployeeId(long id) {
+        // TODO Auto-generated method stub
+        return empRepo.findById(id);
+    }
+
+
+    public void delete(Employee theEmp) {
+        empRepo.delete(theEmp);
+
+    }
+
+
+}
