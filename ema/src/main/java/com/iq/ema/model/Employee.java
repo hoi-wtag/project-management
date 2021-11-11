67a1169e7f4e30155f825494bf9015aa89218fbb
Iqbal <iqbal.hossain@welldev.io>
2021-11-11 15:12:36 +0600

    Update Employee.java

--- ema/src/main/java/com/iq/ema/model/Employee.java
+++ ema/src/main/java/com/iq/ema/model/Employee.java
@@ -9,12 +9,8 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long employeeId;
-    @Column(name= "first_name")
     private String firstName;
-    @Column(name= "last_name")
     private String lastName;
-
-    @Column(name= "email_id")
     private String emailId;
 
 //    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.REFRESH,CascadeType.MERGE,CascadeType.PERSIST},
